5359779e08a856b64e50cb7ffdec46647ca66331
DaveSholy <48659370+DaveSholy@users.noreply.github.com>
2019-04-11 19:30:10 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,24 @@ const client = new Discord.Client();
 const prefix = '.'
 
 client.on('ready', () => {
+
+
+var prefix = "!";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`BOT`,"http://twitch.tv/S-F")
   console.log('')
@@ -27,5 +45,6 @@ client.user.setGame(`BOT`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+
 
 client.login(process.env.BOT_TOKEN);
